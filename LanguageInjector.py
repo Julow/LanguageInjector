21ed25f3069738954f9c039bd61fca2558bca121
juloo <juloo.dsi@gmail.com>
2014-09-30 13:57:14 +0200

    Allow to change syntax

--- LanguageInjector.py
+++ LanguageInjector.py
@@ -1,25 +1,38 @@
 import sublime, sublime_plugin, os, plistlib
 
+xml_location = "Packages/julooLanguageInjectorCache/"
+
+def get_xml_path(id):
+	return xml_location + str(id) +".tmLanguage"
+
+def get_full_path(p):
+	return os.path.join(sublime.packages_path(), os.path.normpath(p[9:]))
+
 class LanguageInjectorListener(sublime_plugin.EventListener):
 
-	xml_location = "Packages/julooLanguageInjectorCache/"
-
-	def get_xml_path(self, id):
-		return self.xml_location + str(id) +".tmLanguage"
-
-	def get_full_path(self, theme_path):
-		return os.path.join(sublime.packages_path(), os.path.normpath(theme_path[9:]))
-
 	def on_close(self, view):
+		view.settings().clear_on_change("syntax")
 		if view.settings().has("old_syntax"):
 			view.set_syntax_file(view.settings().get("old_syntax"))
 			view.settings().erase("old_syntax")
-		full_path = self.get_full_path(self.get_xml_path(view.id()))
+		full_path = get_full_path(get_xml_path(view.id()))
 		if os.path.exists(full_path):
 			os.remove(full_path)
 
-	def override_syntax(self, view, syntax_data):
-		plist = plistlib.readPlistFromBytes(syntax_data.encode("UTF-8"))
+	def on_load_async(self, view):
+		view.run_command("language_injector_update")
+
+
+class LanguageInjectorUpdateCommand(sublime_plugin.TextCommand):
+
+	def override_syntax(self):
+		self.view.settings().clear_on_change("syntax")
+		if self.view.settings().has("old_syntax"):
+			syntax_file = self.view.settings().get("old_syntax")
+		else:
+			syntax_file = self.view.settings().get("syntax")
+			self.view.settings().set("old_syntax", syntax_file)
+		plist = plistlib.readPlistFromBytes(sublime.load_resource(syntax_file).encode("UTF-8"))
 		settings = sublime.load_settings("LanguageInjector.sublime-settings")
 		patterns = settings.get("patterns")
 		for reg in patterns.keys():
@@ -33,7 +46,7 @@ class LanguageInjectorListener(sublime_plugin.EventListener):
 					if param == "parent":
 						valid = False
 						for parent in patterns[reg]["parent"]:
-							if view.match_selector(0, parent):
+							if self.view.match_selector(0, parent):
 								valid = True
 					elif param == "scope":
 						p["name"] = patterns[reg]["scope"]
@@ -43,17 +56,17 @@ class LanguageInjectorListener(sublime_plugin.EventListener):
 					p["captures"] = captures
 			if valid:
 				plist["patterns"].append(p)
-		if not os.path.exists(self.get_full_path(self.xml_location)):
-			os.mkdir(self.get_full_path(self.xml_location))
-		with open(self.get_full_path(self.get_xml_path(view.id())), "wb") as f:
+		if not os.path.exists(get_full_path(xml_location)):
+			os.mkdir(get_full_path(xml_location))
+		with open(get_full_path(get_xml_path(self.view.id())), "wb") as f:
 			plistlib.writePlist(plist, f)
-		view.set_syntax_file(self.get_xml_path(view.id()))
+		self.view.set_syntax_file(get_xml_path(self.view.id()))
+		self.view.settings().add_on_change("syntax", self.syntax_change)
 
-	def on_load_async(self, view):
-		if view.settings().has("old_syntax"):
-			syntax_file = view.settings().get("old_syntax")
-		else:
-			syntax_file = view.settings().get("syntax")
-			view.settings().set("old_syntax", syntax_file)
-		self.override_syntax(view, sublime.load_resource(syntax_file))
-		# TODO view.settings().add_on_change("syntax", view.run_command())
+	def syntax_change(self):
+		if self.view.settings().has("old_syntax"):
+			self.view.settings().erase("old_syntax")
+		self.override_syntax()
+
+	def run(self, edit, **args):
+		self.override_syntax()
